3c8897c0d5e1e7fe271dec200535dea51bd26f1b
Lassi Niinikorpi <lassi.n94@gmail.com>
2021-04-24 22:02:39 +0300

    - Enemy moving working - Enemy aiming working

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -11,6 +11,7 @@ public enum NewDirectionType
 }
 public class EnemyMovement : MonoBehaviour
 {
+    public float speed = 1;
     [Tooltip("How the enemy picks its new destination when target or wall is reached")]
     public NewDirectionType newDirectionType;
     [Tooltip("If wait time before new target is random")]
@@ -18,36 +19,99 @@ public class EnemyMovement : MonoBehaviour
     public float randomWaitMin;
     public float randomWaitMax;
     public float waitTime;
+    public float newTargetDistance;
+    public bool ifTargetDistanceRandom;
+    public float targetDistanceMax;
+    public LayerMask layerMask;
 
-    List<int> directions = new List<int> {0,45,90,135,180,225,270,315,360 };
-    // Start is called before the first frame update
-    void Start()
+    Vector3 target;
+    float canNewTarget;
+    bool inTarget;
+
+    List<int> directions = new List<int> { 0, 45, 90, 135, 180, 225, 270, 315, 360 };
+
+    private void Start()
     {
-        
+        NewTarget();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(inTarget)
+        {
+            if(Time.time >= canNewTarget)
+            {
+                NewTarget();
+            }
+        }
     }
 
     public void NewTarget()
     {
+        inTarget = false;
         Vector3 direction = new Vector3();
         switch (newDirectionType)
         {
             case NewDirectionType.Random:
-                direction = new Vector3(0, direction[Random.Range(0, directions.Count - 1)], 0);
+                direction = new Vector3(0, directions[Random.Range(0, directions.Count - 1)], 0);
                 break;
             case NewDirectionType.AlwaysLeft:
+                direction = new Vector3(0, transform.localEulerAngles.y - 45, 0);
                 break;
             case NewDirectionType.AlwaysRight:
+                direction = new Vector3(0, transform.localEulerAngles.y + 45, 0);
                 break;
             case NewDirectionType.Opposite:
+                direction = new Vector3(0, transform.localEulerAngles.y + 180, 0);
                 break;
             default:
                 break;
         }
+        transform.localEulerAngles = direction;
+        float distance;
+        if (ifTargetDistanceRandom)
+        {
+            distance = Random.Range(0, targetDistanceMax);
+        }
+        else
+        {
+            distance = newTargetDistance;
+        }
+        RaycastHit hit;
+        if (Physics.BoxCast(transform.position, transform.localScale / 2, transform.forward, out hit, transform.rotation, distance, layerMask))
+        {
+            distance = hit.distance - .7f;
+        }
+        target = transform.position + transform.forward * distance;
+        StartCoroutine(MoveToTarget(target));
+    }
+
+    IEnumerator MoveToTarget(Vector3 targetVector)
+    {
+        while (transform.position.x != targetVector.x || transform.position.z != targetVector.z)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetVector, step);
+            yield return new WaitForSeconds(.002f);
+        }
+        CalculateNewWaitTime();
+    }
+
+    void CalculateNewWaitTime()
+    {
+        print("aikaa");
+        if (ifRandomWaitTime)
+        {
+            canNewTarget = Random.Range(randomWaitMin, randomWaitMax) + Time.time;
+        }
+        else
+        {
+            canNewTarget = waitTime + Time.time;
+        }
+        inTarget = true;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(target, transform.localScale);
     }
 }
---
+++ Assets/Scripts/Enemy/EnemyShoot.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public Transform bulletSpawn;
+    public GameObject bulletPrefab;
+    public GameObject barrel;
+    [Tooltip("Check if you want the turret to face player")]
+    public bool rotateToPlayer;
+    public AudioSource audioSource;
+
+    [Header("Turret stats")]
+    [Tooltip("Bullets per second")]
+    public float fireRate = 1;
+    public float bulletSpeed = 3;
+    public float turnSpeed = 1;
+    public GameObject player;
+
+    float _canShoot;
+    void Start()
+    {
+        _canShoot = 1 / fireRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= _canShoot)
+        {
+            //Shoot();
+            _canShoot = Time.time + (1 / fireRate);
+        }
+        TurnTurret();
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        //bullet.GetComponent<Bullet>().speed = bulletSpeed;
+        audioSource.Play();
+    }
+
+    void TurnTurret()
+    {
+        if (rotateToPlayer)
+        {
+            Vector3 direction = player.transform.position - barrel.transform.position;
+
+            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            Vector3 directionVector = new Vector3(0, angle, 0);
+            float step = .2f * turnSpeed;
+            barrel.transform.rotation = Quaternion.RotateTowards(barrel.transform.rotation, Quaternion.Euler(directionVector), step);
+
+        }
+    }
+}
