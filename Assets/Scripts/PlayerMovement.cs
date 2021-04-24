b53fc97ac32a79bd153653d8f14b9560494a1844
Lassi Niinikorpi <lassi.n94@gmail.com>
2021-04-24 12:15:35 +0300

    - Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField, Min(0f)]
+    private float movementSpeed = 10f;
+    [SerializeField, Min(0f)]
+    private float turnSpeed = 10f;
+    [SerializeField]
+    private Rigidbody rb;
+
+    private Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    void MovePlayer()
+    {
+        Vector3 velocity = new Vector3(0,0 , movement.y) * movementSpeed * Time.fixedDeltaTime;
+        velocity = transform.TransformDirection(velocity);
+        rb.velocity = velocity;
+    }
+    
+    void RotatePlayer()
+    {
+        Vector3 turnEuler = new Vector3(0, movement.x, 0) * turnSpeed * Time.fixedDeltaTime;
+        rb.angularVelocity = turnEuler;
+    }
+}
