5c3989994aee33c7c7676b966aac999e425c61e7
Lassi Niinikorpi <lassi.n94@gmail.com>
2021-04-25 20:33:39 +0300

    - enemy above ground added

--- Assets/Scripts/Enemy/EnemyData.cs
+++ Assets/Scripts/Enemy/EnemyData.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class EnemyData : MonoBehaviour
 {
+    public float aboveGround;
+
+    private void Awake()
+    {
+        aboveGround = GetComponent<BoxCollider>().size.y / 2;
+    }
     // Start is called before the first frame update
     void Start()
     {
