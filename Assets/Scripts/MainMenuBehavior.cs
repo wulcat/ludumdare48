537d904a1f654a580d1584e1bde7ca77b92b7ef1
Jamideus <maijami@gmail.com>
2021-04-26 09:51:19 +0300

    Main Menu tweaks

--- Assets/Scripts/MainMenuBehavior.cs
+++ Assets/Scripts/MainMenuBehavior.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 
 public class MainMenuBehavior : MonoBehaviour
 {
+    public Image mute;
+    public Sprite[] mutes;
+
     public static void NewGame()
     {
         SceneManager.LoadScene("DemoScene"); //assuming level 1 name
@@ -20,8 +23,9 @@ public class MainMenuBehavior : MonoBehaviour
 #endif
     }
 
-    public static void Mute()
+    public void Mute()
     {
         AudioManager.instance.Mute();
+        mute.sprite = AudioManager.instance.mute ? mutes[1] : mutes[0];
     }
 }
